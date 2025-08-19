82fab8fcc0884a6edf4c837b384a5990c694dcaa
Umesh Kumawat <umeshkumawat06624@gmail.com>
2025-08-19 14:19:42 +0530

    conrole flow variable pass

--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -4,10 +4,21 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Chai from './Chai.jsx'
 
+
+const userName = 'Umesh Kumawat'
+
+// if .jsx is a function so we can called it and pass in rendering 
+function Main(){
+  return(
+    <h1>Welcome from main level function</h1>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <>
     <App />
     <Chai />
-    <h1>Thanks for visiting in our</h1>
+    <h1>Thanks for visiting in our site {userName}</h1> 
+    {/* <Main /> */}
   </>,
 )
